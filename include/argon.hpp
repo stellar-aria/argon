c128976e15b5668243166b099b28415ca59d1765
Katherine Whitlock <kate@skylinesynths.nyc>
2023-09-30 21:09:42 -0400

    Successfully runs for NEON (VFPv3)

---
+++ include/argon.hpp
@@ -0,0 +1,452 @@
+#pragma once
+#include <arm_neon.h>
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+#include "neon.hpp"
+#include "neon/helpers.hpp"
+
+#ifdef __clang__
+#define ace [[gnu::always_inline]] constexpr
+#else
+#define ace [[gnu::always_inline]] inline
+#endif
+
+namespace argon {
+namespace impl {
+template <typename type>
+struct Vec64 {};
+template <typename type>
+struct Vec128 {};
+
+// clang-format off
+template <> struct Vec64<int8_t> {using type = int8x8_t; };
+template <> struct Vec128<int8_t>  {using type = int8x16_t; };
+
+template <> struct Vec64<uint8_t> {using type = uint8x8_t; };
+template <> struct Vec128<uint8_t>  {using type = uint8x16_t; };
+
+template <> struct Vec64<int16_t> {using type = int16x4_t; };
+template <> struct Vec128<int16_t>  {using type = int16x8_t; };
+
+template <> struct Vec64<uint16_t> {using type = uint16x4_t; };
+template <> struct Vec128<uint16_t>  {using type = uint16x8_t; };
+
+template <> struct Vec64<int32_t> {using type = int32x2_t; };
+template <> struct Vec128<int32_t>  {using type = int32x4_t; };
+
+template <> struct Vec64<uint32_t> {using type = uint32x2_t; };
+template <> struct Vec128<uint32_t>  {using type = uint32x4_t; };
+
+template <> struct Vec64<int64_t> {using type = int64x1_t; };
+template <> struct Vec128<int64_t>  {using type = int64x2_t; };
+
+template <> struct Vec64<uint64_t> {using type = uint64x1_t; };
+template <> struct Vec128<uint64_t>  {using type = uint64x2_t; };
+
+template <> struct Vec64<float> {using type = float32x2_t; };
+template <> struct Vec128<float>  {using type = float32x4_t; };
+
+template <typename T> struct NextLarger;
+template <> struct NextLarger<int8x8_t> {using type = int16_t; };
+template <> struct NextLarger<uint8x8_t> {using type = uint16_t; };
+template <> struct NextLarger<int16x4_t> {using type = int32_t; };
+template <> struct NextLarger<uint16x4_t> {using type = uint32_t; };
+template <> struct NextLarger<int32x2_t> {using type = int64_t; };
+template <> struct NextLarger<uint32x2_t> {using type = uint64_t; };
+
+template <> struct NextLarger<int8x16_t> {using type = int16_t; };
+template <> struct NextLarger<uint8x16_t> {using type = uint16_t; };
+template <> struct NextLarger<int16x8_t> {using type = int32_t; };
+template <> struct NextLarger<uint16x8_t> {using type = uint32_t; };
+template <> struct NextLarger<int32x4_t> {using type = int64_t; };
+template <> struct NextLarger<uint32x4_t> {using type = uint64_t; };
+
+template <> struct NextLarger<int8_t> {using type = int16_t; };
+template <> struct NextLarger<uint8_t> {using type = uint16_t; };
+template <> struct NextLarger<int16_t> {using type = int32_t; };
+template <> struct NextLarger<uint16_t> {using type = uint32_t; };
+template <> struct NextLarger<int32_t> {using type = int64_t; };
+template <> struct NextLarger<uint32_t> {using type = uint64_t; };
+
+// clang-format on
+
+template <typename T>
+concept arithmetic = std::is_arithmetic_v<T>;
+
+template <typename vector_type>
+class FloatCommon {
+  using base_type = neon::NonVec<vector_type>::type;
+
+ public:
+  using T = FloatCommon<vector_type>;
+};
+
+template <typename vector_type>
+class Lane;
+
+template <typename vector_type>
+class Common {
+  using base_type = neon::NonVec<vector_type>::type;
+
+ public:
+  using T = Common<vector_type>;
+  static constexpr size_t lanes = (neon::is_quadword_v<vector_type> ? 16 : 8) / sizeof(base_type);
+
+  constexpr Common() : vec_{0} {};
+  constexpr Common(vector_type vector) : vec_(vector){};
+  constexpr Common(base_type base) : vec_(LoadCopy(base)){};
+  constexpr Common(base_type const* base_ptr) : vec_(Load(base_ptr)){};
+
+  ace T operator+(T b) const { return Add(b); }
+  ace T operator-(T b) const { return Subtract(b); }
+  ace T operator*(T b) const { return Multiply(b); }
+  ace T operator/(T b) const { return Divide(b); }
+
+  ace T operator+=(T b) {
+    vec_ = Add(b);
+    return vec_;
+  }
+
+  ace T operator-=(T b) {
+    vec_ = Subtract(b);
+    return vec_;
+  }
+
+  ace T operator==(T b) const { return Equal(b); }
+  ace T operator<(T b) const { return LessThan(b); }
+  ace T operator>(T b) const { return GreaterThan(b); }
+  ace T operator<=(T b) const { return LessThanOrEqual(b); }
+  ace T operator>=(T b) const { return GreaterThanOrEqual(b); }
+
+  ace T operator++() const { return Add(1); }
+  ace T operator--() const { return Subtract(1); }
+
+  ace T operator&(T b) const { return AndBitwise(b); }
+  ace T operator|(T b) const { return OrBitwise(b); }
+  ace T operator^(T b) const { return XorBitwise(b); }
+  ace T operator~() const { return NotBitwise(); }
+
+  ace Lane<vector_type> operator[](const int i) const { return Lane{vec_, i}; }
+
+  ace T operator>>(const int i) const { return ShiftRight(i); }
+  ace T operator<<(const int i) const { return ShiftLeft(i); }
+
+  ace operator vector_type() const { return vec_; }
+
+  ace std::array<base_type, lanes> as_array() {
+    std::array<base_type, lanes> out{0};
+    Store(out.data());
+    return out;
+  }
+
+  ace vector_type vec() const { return vec_; }
+
+  ace base_type Get(const int i) const { return neon::get<vector_type>(vec_, i); }
+
+  ace base_type Set(const int i, base_type b) const { return neon::set(vec_, i, b); }
+
+  ace T ShiftRight(const int i) const { return neon::shift_right(vec_, i); }
+
+  ace T ShiftLeft(const int i) const { return neon::shift_left(vec_, i); }
+
+  ace T Add(T b) const { return neon::add(vec_, b); }
+
+  ace T AddHalve(T b) const { return neon::add_halve(vec_, b); }
+
+  ace T AddHalveRound(T b) const { return neon::add_halve_round(vec_, b); }
+
+  ace T AddSaturate(T b) const { return neon::add_saturate(vec_, b); }
+
+  ace T Multiply(T b) const { return neon::multiply(vec_, b); }
+
+  ace T MultiplyAdd(T b, T c) const { return neon::multiply_add(vec_, b, b); }
+
+  ace T MultiplySubtract(T b, T c) const { return neon::multiply_subtract(vec_, b, b); }
+
+  ace T Subtract(T b) const { return neon::subtract(vec_, b); }
+
+  ace T SubtractHigh(T b) const { return neon::subtract_high(vec_, b); }
+
+  ace T SubtractSaturate(T b) const { return neon::subtract_saturate(vec_, b); }
+
+  ace T SubtractAbs(T b) const { return neon::subtract_abs(vec_, b); }
+
+  ace T SubtractAbsAdd(T b, T c) const { return neon::subtract_abs_add(vec_, b, c); }
+
+  ace T Max(T b) const { return neon::max(vec_, b); }
+
+  ace T Min(T b) const { return neon::min(vec_, b); }
+
+  ace T AddPairwise(T b) const { return neon::add_pairwise(vec_, b); }
+
+  ace T MaxPairwise(T b) const { return neon::max_pairwise(vec_, b); }
+
+  ace T MinPairwise(T b) const { return neon::min_pairwise(vec_, b); }
+
+  ace T Equal(T b) const { return neon::equal(vec_, b); }
+
+  ace T GreaterThanOrEqual(T b) const { return neon::greater_than_or_equal(vec_, b); }
+
+  ace T LessThanOrEqual(T b) const { return neon::less_than_or_equal(vec_, b); }
+
+  ace T GreaterThan(T b) const { return neon::greater_than(vec_, b); }
+
+  ace T LessThan(T b) const { return neon::less_than(vec_, b); }
+
+  ace T AndTestNonzero(T b) const { return neon::and_test_nonzero(vec_, b); }
+
+  ace T ShiftLeft(Common<typename neon::make_signed<vector_type>::type> b) const { return neon::shift_left(vec_, b); }
+
+  ace T ShiftLeftSaturate(Common<typename neon::make_signed<vector_type>::type> b) const {
+    return neon::shift_left_saturate(vec_, b);
+  }
+
+  template <int n>
+  ace T ShiftLeftSaturate() const {
+    return neon::shift_left_saturate<n>(vec_);
+  }
+
+  ace T ShiftLeftRound(T b) const { return neon::shift_left_round(vec_, b); }
+
+  ace T ShiftLeftRoundSaturate(T b) const { return neon::shift_left_round_saturate(vec_, b); }
+
+  template <int n>
+  ace uint16x8_t ShiftLeftLong() const {
+    return neon::shift_left_long<n>(vec_);
+  }
+
+  template <int n>
+  ace T ShiftLeftInsert(T b) const {
+    return neon::shift_left_insert<n>(vec_, b);
+  }
+
+  template <int n>
+  ace T ShiftRight() const {
+    return neon::shift_right<n>(vec_);
+  }
+
+  template <int n>
+  ace T ShiftRightRound() const {
+    return neon::shift_right_round<n>(vec_);
+  }
+
+  template <int n>
+  ace T ShiftRightAccumulate(T b) const {
+    return neon::shift_right_accumulate<n>(vec_, b);
+  }
+
+  template <int n>
+  ace T ShiftRightAccumulateRound(T b) const {
+    return neon::shift_right_accumulate_round<n>(vec_, b);
+  }
+
+  template <int n>
+  ace T ShiftRightInsert(T b) const {
+    return neon::shift_right_insert<n>(vec_, b);
+  }
+
+  ace static T Load(base_type const* ptr) { return neon::load1<vector_type>(ptr); }
+  ace static T Load_2(base_type const* ptr) { return neon::load2<vector_type>(ptr); }
+  ace static T Load_3(base_type const* ptr) { return neon::load3<vector_type>(ptr); }
+  ace static T Load_4(base_type const* ptr) { return neon::load4<vector_type>(ptr); }
+
+  ace void Store(base_type* ptr) { neon::store1<vector_type>(ptr, vec_); }
+  ace void Store2(base_type* ptr) { neon::store2<vector_type>(ptr, vec_); }
+  ace void Store3(base_type* ptr) { neon::store3<vector_type>(ptr, vec_); }
+  ace void Store4(base_type* ptr) { neon::store4<vector_type>(ptr, vec_); }
+
+  template <int lane>
+  ace void StoreLane(base_type* ptr) {
+    neon::store1<lane>(ptr, vec_);
+  }
+
+  template <int lane>
+  ace void StoreLane2(base_type* ptr) {
+    neon::store2<lane>(ptr, vec_);
+  }
+
+  template <int lane>
+  ace void StoreLane3(base_type* ptr) {
+    neon::store3<lane>(ptr, vec_);
+  }
+
+  template <int lane>
+  ace void StoreLane4(base_type* ptr) {
+    neon::store4<lane>(ptr, vec_);
+  }
+
+  ace static T LoadCopy(base_type b) { return neon::duplicate_element<vector_type>(b); }
+  ace static T Move(base_type b) { return neon::move<vector_type>(b); }
+
+  ace T NotBitwise() const { return neon::not_bitwise(vec_); }
+
+  ace T AndBitwise(T b) const { return neon::and_bitwise(vec_, b); }
+
+  ace T OrBitwise(T b) const { return neon::or_bitwise(vec_, b); }
+
+  ace T XorBitwise(T b) const { return neon::xor_bitwise(vec_, b); }
+
+  ace T OrNotBitwise(T b) const { return neon::or_not_bitwise(vec_, b); }
+
+  ace typename neon::make_signed<vector_type>::type CountLeadingSignBits() const {
+    return neon::count_leading_sign_bits(vec_);
+  }
+
+  ace T CountLeadingZeroBits() const { return neon::count_leading_zero_bits(vec_); }
+
+  ace T CountActiveBits() const { return neon::count_active_bits(vec_); }
+
+  ace T ClearBitwise(T b) const { return neon::clear_bitwise(vec_, b); }
+
+  ace T SelectBitwise(T b, T c) const { return neon::select_bitwise(vec_, b, b); }
+
+  template <int lane>
+  ace T DuplicateElement() const {
+    return neon::duplicate_element(vec_, lane);
+  }
+
+  template <int lane>
+  ace uint8x16_t DuplicateElement() const {
+    return neon::duplicate_element(vec_, lane);
+  }
+
+  ace uint8x16_t Combine(T high) const { return neon::combine(vec_, high); }
+
+  template <int lane>
+  ace uint8_t Get() const {
+    return neon::get(vec_, lane);
+  }
+
+  template <int n>
+  ace T Extract(T b) const {
+    return neon::extract(vec_, b, n);
+  }
+
+  ace T Reverse64bit() const { return neon::reverse_64bit(vec_); }
+
+  ace T Reverse32bit() const { return neon::reverse_32bit(vec_); }
+
+  ace T Reverse16bit() const { return neon::reverse_16bit(vec_); }
+
+  ace uint8x8x2_t Zip(T b) const { return neon::zip(vec_, b); }
+
+  uint8x8x2_t Unzip(T b) { return neon::unzip(vec_, b); }
+
+  ace uint8x8x2_t Transpose(T b) const { return neon::transpose(vec_, b); }
+
+ protected:
+  vector_type vec_;
+};
+
+template <typename vector_type>
+class Lane {
+  using base_type = neon::NonVec<vector_type>::type;
+
+ public:
+  using T = Lane<vector_type>;
+
+  ace Lane(vector_type vec, const int lane) : vec_(vec), lane_(lane){};
+
+  ace operator neon::NonVec<vector_type>::type() { return neon::get(vec_, lane_); }
+
+  ace Common<vector_type> operator=(base_type b) { return neon::set(vec_, lane_, b); }
+
+ private:
+  vector_type vec_;
+  const int lane_;
+};
+
+}  // namespace impl
+
+template <typename base>
+class Neon128;
+
+template <typename base>
+class Neon64 : public impl::Common<typename impl::Vec64<base>::type> {
+  using T = impl::Common<typename impl::Vec64<base>::type>;
+  using next_larger_type = impl::NextLarger<base>::type;
+
+ public:
+  using vector_type = impl::Vec64<base>::type;
+  using T::T;
+
+  static_assert(neon::is_doubleword_v<vector_type>);
+
+  static constexpr size_t bytes = 8;
+  static constexpr size_t lanes = bytes / sizeof(base);
+
+  ace Neon64(T&& in) : T(in){};
+
+  ace Neon128<next_larger_type> AddLong(T b) const { return neon::add_long(this->vec_, b); }
+  ace Neon128<next_larger_type> MultiplyLong(T b) const { return neon::multiply_long(this->vec_, b); }
+  ace Neon128<next_larger_type> SubtractLong(T b) const { return neon::subtract_long(this->vec_, b); }
+  ace Neon128<next_larger_type> MoveLong() const { return neon::move_long(this->vec_); }
+
+  ace Neon128<next_larger_type> SubtractAbsLong(T b) const { return neon::subtract_abs_long(this->vec_, b); }
+  ace Neon128<next_larger_type> AddPairwiseLong() const { return neon::add_pairwise_long(this->vec_); }
+
+  ace T TableLookup1(T idx) const { return neon::table_lookup1(this->vec_, idx); }
+  ace T TableLookupExtension1(T b, T idx) const { return neon::table_lookup_extension1(this->vec_, b, idx); }
+  ace T TableLookupExtension2(uint8x8x2_t b, T idx) const { return neon::table_lookup_extension2(this->vec_, b, idx); }
+  ace T TableLookupExtension3(uint8x8x3_t b, T idx) const { return neon::table_lookup_extension3(this->vec_, b, idx); }
+  ace T TableLookupExtension4(uint8x8x4_t b, T idx) const { return neon::table_lookup_extension4(this->vec_, b, idx); }
+};
+
+template <typename base_type>
+class Neon128 : public impl::Common<typename impl::Vec128<base_type>::type> {
+  using T = impl::Common<typename impl::Vec128<base_type>::type>;
+
+ public:
+  using vector_type = impl::Vec128<base_type>::type;
+  constexpr Neon128() : T(){};
+  constexpr Neon128(vector_type vector) : T(vector){};
+  constexpr Neon128(base_type base) : T(base){};
+  constexpr Neon128(base_type const* base_ptr) : T(base_ptr){};
+  constexpr Neon128(T&& in) : T(in){};
+
+  static_assert(neon::is_quadword_v<vector_type>);
+
+  static constexpr size_t bytes = 16;
+  static constexpr size_t lanes = bytes / sizeof(base_type);
+
+  ace Neon64<vector_type> GetHigh() { return vget_high_u8(this->vec_); }
+  ace Neon64<vector_type> GetLow() { return vget_low_u8(this->vec_); }
+};
+
+template <typename NeonType, neon::is_vector_type V>
+ace NeonType reinterpret(impl::Common<V> in) {
+  static_assert(!std::is_same_v<typename NeonType::vector_type, V>);
+  return NeonType{neon::reinterpret<typename NeonType::vector_type>(in.vec())};
+}
+
+template <typename NeonType, neon::is_vector_type V>
+ace NeonType reinterpret(V in) {
+  static_assert(!std::is_same_v<typename NeonType::vector_type, V>);
+  return NeonType{neon::reinterpret<typename NeonType::vector_type>(in)};
+}
+
+template <neon::is_vector_type V>
+ace V operator+(typename neon::NonVec<V>::type a, impl::Common<V> b) {
+  return b.Add(b);
+}
+
+template <neon::is_vector_type V>
+ace V operator-(typename neon::NonVec<V>::type a, impl::Common<V> b) {
+  return impl::Common<V>{a}.Subtract(b);
+}
+
+template <neon::is_vector_type V>
+ace V operator*(typename neon::NonVec<V>::type a, impl::Common<V> b) {
+  return b.Multiply(a);
+}
+
+template <neon::is_vector_type V>
+ace V operator/(typename neon::NonVec<V>::type a, impl::Common<V> b) {
+  return impl::Common<V>{a}.Divide(b);
+}
+
+}  // namespace argon
+#undef ace
---
+++ test/main.cpp
@@ -0,0 +1,159 @@
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <numbers>
+#include "argon.hpp"
+
+using namespace argon;
+
+namespace skyline::dsp::osc {
+class CosineOscillator {
+ public:
+  enum class Mode { APPROX, EXACT };
+  ~CosineOscillator() = default;
+
+  template <Mode mode = Mode::APPROX>
+  inline CosineOscillator(float frequency) {
+    if constexpr (mode == Mode::APPROX) {
+      InitApproximate(frequency);
+    } else {
+      iir_coefficient_ = 2.0f * std::cos(2.0f * std::numbers::pi_v<float> * frequency);
+      initial_amplitude_ = iir_coefficient_ * 0.25f;
+    }
+    Start();
+  }
+
+  inline void InitApproximate(float frequency) {
+    float sign = 16.0f;
+    frequency -= 0.25f;
+    if (frequency < 0.0f) {
+      frequency = -frequency;
+    } else if (frequency > 0.5f) {
+      frequency -= 0.5f;
+    } else {
+      sign = -16.0f;
+    }
+    iir_coefficient_ = sign * frequency * (1.0f - 2.0f * frequency);
+    initial_amplitude_ = iir_coefficient_ * 0.25f;
+  }
+
+  inline void Start() {
+    y[0] = initial_amplitude_;
+    y[1] = 0.5f;
+  }
+
+  inline float value() const { return y[0] + 0.5f; }
+
+  inline float Next() {
+    float temp = y[1];
+    y[1] = iir_coefficient_ * y[1] - y[0];
+    y[0] = temp;
+    return temp + 0.5f;
+  }
+
+ private:
+  float y[2];
+  float iir_coefficient_;
+  float initial_amplitude_;
+};
+
+class NeonCosineOscillator {
+  static constexpr size_t channels = 4;
+
+ public:
+  enum class Mode { APPROX, EXACT };
+
+  template <Mode mode = Mode::APPROX>
+  constexpr NeonCosineOscillator(std::array<float, 4> frequencies) {
+    if constexpr (mode == Mode::APPROX) {
+      InitApproximate(frequencies);
+    } else {
+      for (size_t i = 0; i < channels; ++i) {
+        iir_coefficient_[i] = 2.0f * std::cos(2.0f * std::numbers::pi_v<float> * frequencies[i]);
+        initial_amplitude_[i] = iir_coefficient_[i] * 0.25f;
+      }
+    }
+    Start();
+  }
+  ~NeonCosineOscillator() = default;
+
+  inline void InitApproximate(std::array<float, channels> frequencies) {
+    Neon128<float> sign = 16.0f;
+    for (size_t i = 0; i < frequencies.size(); ++i) {
+      float& frequency = frequencies[i];
+      frequency -= 0.25f;
+      if (frequency < 0.0f) {
+        frequency = -frequency;
+      } else if (frequency > 0.5f) {
+        frequency -= 0.5f;
+      } else {
+        sign[i] = -16.0f;
+      }
+    }
+    Neon128<float> frequency = frequencies.data();
+    iir_coefficient_ = sign * frequency * (1.0f - (2.0f * frequency));
+    initial_amplitude_ = iir_coefficient_ * 0.25f;
+  }
+
+  constexpr void Start() {
+    y_0 = initial_amplitude_;
+    y_1 = 0.5f;
+  }
+
+  [[nodiscard]] inline Neon128<float> values() const { return y_0 + 0.5f; }
+
+  inline Neon128<float> Next() {
+    Neon128<float> temp = y_1;
+    y_1 = iir_coefficient_ * y_1 - y_0;
+    y_0 = temp;
+    return temp + 0.5f;
+  }
+
+ private:
+  Neon128<float> y_0;
+  Neon128<float> y_1;
+  Neon128<float> iir_coefficient_;
+  Neon128<float> initial_amplitude_;
+};
+}  // namespace skyline::dsp::osc
+
+#include <chrono>
+
+void print_time(char const* name, auto start, auto stop) {
+  std::chrono::duration<double, std::milli> fp_ms{stop - start};
+  std::cout << name << " took " << fp_ms << " \n";
+}
+
+int main() {
+  size_t iterations = 8192 * 8192 * 2;
+  printf("Running %d iterations...\n", iterations);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  skyline::dsp::osc::CosineOscillator co{0.5f / 5000};
+  float f = 0;
+  for (size_t i = 0; i < iterations; ++i) {
+    f = co.Next();
+  }
+  auto stop = std::chrono::high_resolution_clock::now();
+  print_time("VFPv3", start, stop);
+  std::cout << "Result: " << f << std::endl;
+
+  // for (size_t i = 0; i < 4097; ++i) {
+  //     auto n = nco.Next();
+  //     auto f = co.Next();
+  //     if (i % 512 == 0) {
+  //       printf("Step %4d: VFPv3 {%.6f} vs NEON {%.6f, %.6f, %.6f, %.6f}\n", i, f, float(n[0]), float(n[1]),
+  //       float(n[2]), float(n[3]));
+  //     }
+  // }
+
+  start = std::chrono::high_resolution_clock::now();
+  skyline::dsp::osc::NeonCosineOscillator nco{{0.5f / 5000, 0.75f / 5000, 1.0f / 5000, 1.5f / 5000}};
+  Neon128<float> n;
+  for (size_t i = 0; i < iterations; ++i) {
+    n = nco.Next();
+  }
+  stop = std::chrono::high_resolution_clock::now();
+  print_time("NEON", start, stop);
+  printf("Result: {%.6f, %.6f, %.6f, %.6f}\n", float(n[0]), float(n[1]), float(n[2]), float(n[3]));
+}
